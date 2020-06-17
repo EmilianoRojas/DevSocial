46bbbad2be557804c5df167dc8a98b5f3cb55c92
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-17 13:56:29 -0400

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,79 @@ router.get("/users/:user_id", async (req, res) => {
   }
 });
 
+//@route   DELETE api/profile
+//@desc    Delete profile, user & posts
+//@access  Private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove user posts
+
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route   PUT api/profile/experience
+//@desc    Add profile experience
+//@access  Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is Required").not().isEmpty(),
+      check("company", "Company is Required").not().isEmpty(),
+      check("from", "From date is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
