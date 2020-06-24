641ec735e937f679be3e655dd5306fca86e4c3ef
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-23 23:04:10 -0400

    redux alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
 
 
-const Register = () => {
+const Register = (props) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -19,7 +22,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      props.setAlert('Passwords do not match', 'danger');
     } else {
       console.log('success')
     }
@@ -72,10 +75,10 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Register
+export default connect(null, { setAlert })(Register);
