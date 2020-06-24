2dbd86c2a384d984bad65863586eb9a995b9b541
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-23 23:38:30 -0400

    alert redux action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -22,7 +23,7 @@ const Register = (props) => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('success')
     }
@@ -79,6 +80,10 @@ const Register = (props) => {
       </p>
     </Fragment>
   );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
 };
 
 export default connect(null, { setAlert })(Register);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import alert from './alert'
 
 export default combineReducers({
-
+  alert
 });
