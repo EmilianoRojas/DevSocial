64fceb2449150e0c59b2f6f2ee05a0b12bbec917
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-27 13:23:57 -0400

    create profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profile'
@@ -11,11 +12,16 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 
   return loading && profile === null ? <Spinner /> : <Fragment>
     <h1 className="large text-primary">Dashboard</h1>
-    <p className="lead"><i className="fas fa-user"></i>Welcome {user && user.name}</p>
-
-
-
-
+    <p className="lead"><i className="fas fa-user"></i> Welcome {user && user.name}</p>
+    {profile !== null ?
+      (<Fragment>has</Fragment>) :
+      (<Fragment>
+        <p>You have not yet setup a profile, please add some info</p>
+        <Link to="/create-profile" className="btn btn-primary my-1">
+          Create Profile
+      </Link>
+      </Fragment>)
+    }
   </Fragment>
 }
 
