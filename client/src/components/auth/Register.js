63c3dec025ce1a9554a6f109b39a998add0d7fc0
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-27 08:00:08 -0400

    edit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -29,6 +29,10 @@ const Register = ({ setAlert, register }) => {
       register({ name, email, password })
     }
   };
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />
+  }
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -86,6 +90,11 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
