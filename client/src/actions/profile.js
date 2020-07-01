e3a1a283733694d8728995c46ff5e7afaa245fbd
EmilianoRojas <inversiones.socony@gmail.com>
2020-07-01 18:50:01 -0400

    adding profile viewer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,10 +48,10 @@ export const getProfiles = () => async dispatch => {
 }
 
 //get profile by id
-export const getProfilesById = userId => async dispatch => {
+export const getProfileById = userId => async dispatch => {
 
   try {
-    const res = await axios.get(`/api/profile/user/${userId}`);
+    const res = await axios.get(`/api/profile/users/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
@@ -235,7 +235,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone')) {
     try {
-      const res = await axios.delete('/api/profile');
+      await axios.delete('/api/profile');
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: DELETE_ACCOUNT });
