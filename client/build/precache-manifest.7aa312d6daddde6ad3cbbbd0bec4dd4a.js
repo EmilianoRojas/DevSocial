26512a04a43392224279a6900d75f0e0f9fa4c28
EmilianoRojas <inversiones.socony@gmail.com>
2020-07-03 09:15:47 -0400

    make it better

---
+++ client/build/precache-manifest.7aa312d6daddde6ad3cbbbd0bec4dd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b2a87ad8909b07d8531bdf0d832e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9178800cee67a473020",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "c8246f68a86a223049be",
+    "url": "/static/js/2.5f1e3a31.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.5f1e3a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9178800cee67a473020",
+    "url": "/static/js/main.a663ba7e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
